84cfb633254f831ca9ea0540c77d48b9896f8ea3
Jiri Podivin <jpodivin@gmail.com>
2021-03-21 14:01:53 +0100

    Inverted evaluation.
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- testfuncpy/base_function.py
+++ testfuncpy/base_function.py
@@ -2,9 +2,17 @@ import numpy
 
 class BaseFunction:
 
+    def __init__(self, inverted=False):
+
+        self.inverted=inverted
+
     def __call__(self, *values):
-        
-        return self.__evaluate__(values)
+        evaluation = self.__evaluate__(values)
+
+        if self.inverted:
+            evaluation = 1 / (1 + evaluation)
+
+        return evaluation
 
     def __evaluate__(self, *values):
 
