6128933ad94f636b92a6c89122cbcb7a797c0792
Jiri Podivin <jpodivin@gmail.com>
2021-03-21 13:15:27 +0100

    Initial packaging setup.
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testfuncpy",
+    version="0.0.1",
+    author="Jiří Podivín",
+    author_email="jpodivin@gmail.com",
+    description="Library of various test functions for benchmarking optimization algorithms.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jpodivin/testfuncpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Mathematics"
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy"
+    ]
+)
