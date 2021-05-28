ea05f0e9d56bdf74e4351211a46975e3cc8023d6
Jiri Podivin <jpodivin@gmail.com>
2021-05-28 16:42:24 +0200

    Facilities for functions to reduce input dimensionality
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- tests/test_numeric_functions.py
+++ tests/test_numeric_functions.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from landscapy import numeric_functions
+from landscapy import numeric_functions as nf
 
 
 class TestNumericFunction(unittest.TestCase):
@@ -9,3 +9,9 @@ class TestNumericFunction(unittest.TestCase):
 
         self.test_values = np.ones((100,))
         super(TestNumericFunction, self).setUp()
+
+    def test_holder_table_fn(self):
+
+        function = nf.HolderTableFn()
+
+        result = function(self.test_values)
