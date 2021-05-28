1a95cef11d29ec96d53f95de9b6f0a154e8aae7a
Jiri Podivin <jpodivin@gmail.com>
2021-05-28 10:28:46 +0200

    Renaming the project
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# testfuncpy
+# landscapy
 Library of various test functions for benchmarking optimization algorithms. 
 
 Spun off from my [pystrand](https://github.com/jpodivin/pystrand) project in order to formalize management. 
 
-The name is mostly a placeholder. I couldn't compe up with a better one, or didn't try.
-So it will be subject to change, probably.
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="testfuncpy",
+    name="landscapy",
     version="0.0.1",
     author="Jiří Podivín",
     author_email="jpodivin@gmail.com",
     description="Library of various test functions for benchmarking optimization algorithms.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jpodivin/testfuncpy",
+    url="https://github.com/jpodivin/landscapy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python",
