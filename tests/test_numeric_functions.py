48a5dcd558adeac677b159685828de456b37b663
Jiri Podivin <jpodivin@gmail.com>
2021-05-28 17:27:33 +0200

    Test for Holder table
    Not sure why was that line in HolderTable def?
    
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- tests/test_numeric_functions.py
+++ tests/test_numeric_functions.py
@@ -7,7 +7,7 @@ class TestNumericFunction(unittest.TestCase):
 
     def setUp(self):
 
-        self.test_values = np.ones((100,))
+        self.test_values = np.ones((10,))
         super(TestNumericFunction, self).setUp()
 
     def test_holder_table_fn(self):
@@ -15,3 +15,6 @@ class TestNumericFunction(unittest.TestCase):
         function = nf.HolderTableFn()
 
         result = function(self.test_values)
+
+        self.assertEqual(function.evaluated, 1)
+        self.assertAlmostEqual(result, -0.95016120789)
